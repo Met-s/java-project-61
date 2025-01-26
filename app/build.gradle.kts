ee5550190b481bd04f48a3a326a84b1604dc9090
Met-s <den_va@mail.ru>
2025-01-26 13:02:47 +0300

    Использовал утилиту make и пакет Scanner - ввод пользователем текста в консоль

---
+++ app/build.gradle.kts
@@ -0,0 +1,30 @@
+plugins {
+    application
+    id("java")
+    id ("com.github.ben-manes.versions") version "0.52.0"
+
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ app/src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,14 @@
+package hexlet.code;
+import java.util.Scanner;
+
+public class Cli {
+    public static void scan() {
+        System.out.println("Welcome to the Brain Games!");
+        System.out.print("May I have your name? ");
+        Scanner scan = new Scanner(System.in);
+        String name = scan.next();
+        System.out.println("Hello, " + name + "!");
+        scan.close();
+    }
+
+}
