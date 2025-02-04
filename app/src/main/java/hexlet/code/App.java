446f9f3a4e254fb6782c55f013c538a7beffc8fc
Met-s <den_va@mail.ru>
2025-02-04 17:50:27 +0300

    Игровое меню

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,10 +1,35 @@
 package hexlet.code;
+import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
-        Cli.scan();
+        System.out.print("Please enter the game number and press Enter.\n"
+                + "1 - Greet\n"
+                + "2 - Game Even\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
 
+        Scanner choice = new Scanner(System.in);
+        try {
+            int number = choice.nextInt();
+            if (2 < number || number < 0) {
+                throw new Exception();
+            }
+            switch (number) {
+                case 0:
+                    System.out.println("Goodbye!");
+                    break;
+                case 1:
+                    Cli.scan();
+                    break;
+                case 2:
+                    Even.parityCheck();
+                    break;
+                default:
+            }
+        } catch (Exception e) {
+            System.out.println(("Incorrect input!"));
+        }
     }
 }
-
-
