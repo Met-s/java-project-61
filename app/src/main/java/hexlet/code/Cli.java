267286ceaa291a0a5e477f836e598fbfc090bfed
Met-s <den_va@mail.ru>
2025-02-04 17:51:30 +0300

    Приветствие,знакомство с пользователем

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -1,13 +1,20 @@
 package hexlet.code;
 import java.util.Scanner;
 
+
 public class Cli {
+    public static String nameUser;
     public static void scan() {
+        /**
+         * Getting to know the user
+         */
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner scan = new Scanner(System.in);
-        String name = scan.next();
-        System.out.println("Hello, " + name + "!");
-        scan.close();
+        nameUser = scan.next();
+        System.out.println("Hello, " + nameUser + "!");
+    }
+    public String getName() {
+        return nameUser;
     }
 }
