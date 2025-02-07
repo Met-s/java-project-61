3f280cb1df9b9f2e426a2b8a655e6ec1ecabc48d
Met-s <den_va@mail.ru>
2025-02-07 16:58:45 +0300

    Добавлены общие методы

--- app/src/main/java/hexlet/code/games/Even.java
+++ app/src/main/java/hexlet/code/games/Even.java
@@ -1,36 +1,33 @@
-package hexlet.code;
-import java.util.Scanner;
+package hexlet.code.games;
+
+import hexlet.code.Cli;
+import hexlet.code.Engine;
+import static hexlet.code.Engine.answer;
 
 
 public class Even {
+    /**
+     * Game "Check for parity".
+     * Game essence:
+     * The user is shown a random number,
+     * needs to answer "yes" if the number is even,
+     * or "no" if it is odd:
+     * If the user gives an incorrect answer, the game ends.
+     * The user must give the correct answer to three questions in a row.
+     * Any incorrect input is considered an error.
+     */
     public static void parityCheck() throws Exception {
-        /**
-         * Game "Check for parity".
-         * Game essence:
-         * The user is shown a random number,
-         * needs to answer "yes" if the number is even,
-         * or "no" if it is odd:
-         * If the user gives an incorrect answer, the game ends.
-         * The user must give the correct answer to three questions in a row.
-         * Any incorrect input is considered an error.
-         */
-        int minRandom = 1;
-        int maxRandom = 100;
-        int correctAnswers = 0;
-        Cli.scan();
-        String nameUser = Cli.nameUser;
 
+        int correctAnswers = 0;
+
+        Cli.greeting();
+        String nameUser = Cli.nameUser;
         System.out.println("Answer 'yes' if the number is even, "
                 + "otherwise answer 'no'.");
 
         while (correctAnswers < 3) {
-            int number =
-                    minRandom + (int) (Math.random() * ((maxRandom - minRandom) + 1));
-            System.out.println("Question: " + number);
-            System.out.print("Your answer: ");
-            Scanner scan1 = new Scanner(System.in);
-            String answer = scan1.next();
-
+            int number = Engine.random(100);
+            Engine.game(String.valueOf(number));
             if ((!answer.equals("no")) & (!answer.equals("yes"))) {
                 throw new Exception();
             }
@@ -51,7 +48,7 @@ public class Even {
                     System.out.println("Correct!");
                     correctAnswers++;
                 }
-                if (answer.equals("yes")) { // ok
+                if (answer.equals("yes")) {
                     System.out.println("'yes' is wrong answer ;(. Correct"
                             + " answer was 'no'.\nLet's try again, "
                             + nameUser + "!");
@@ -59,8 +56,6 @@ public class Even {
                 }
             }
         }
-        if (correctAnswers == 3) {
-            System.out.println("Congratulations, " + nameUser + "!");
-        }
+        Engine.finall(correctAnswers);
     }
 }
