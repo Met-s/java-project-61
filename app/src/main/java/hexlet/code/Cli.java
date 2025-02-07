12f7edc7e0e737902155c9d7ff6fe256a7098263
Met-s <den_va@mail.ru>
2025-02-07 16:57:18 +0300

    Переименован метод scan() -> greeting()

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ import java.util.Scanner;
 
 
 public class Cli {
+    /**
+     * Getting to know the user.
+     */
     public static String nameUser;
-    public static void scan() {
-        /**
-         * Getting to know the user
-         */
+    public static void greeting() {
+
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner scan = new Scanner(System.in);
