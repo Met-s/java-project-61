3b0688765b574aecf5daa933f86aeb831340cc89
Met-s <den_va@mail.ru>
2025-02-11 15:08:41 +0300

    В меню добавлен пункт 4, игра GCD "наибольший общий делитель НОД"

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,9 @@
 package hexlet.code;
-import hexlet.code.games.Even;
-import hexlet.code.games.Calc;
 
 import java.util.Scanner;
+import hexlet.code.games.Even;
+import hexlet.code.games.Calc;
+import hexlet.code.games.Gcd;
 
 
 public class App {
@@ -11,13 +12,14 @@ public class App {
                 + "1 - Greet\n"
                 + "2 - Game Even\n"
                 + "3 - Game Calc\n"
+                + "4 - Game GCD\n"
                 + "0 - Exit\n"
                 + "Your choice: ");
 
         Scanner choice = new Scanner(System.in);
         try {
             int number = choice.nextInt();
-            if (3 < number || number < 0) {
+            if (4 < number || number < 0) {
                 throw new Exception();
             }
             switch (number) {
@@ -32,6 +34,10 @@ public class App {
                     break;
                 case 3:
                     Calc.calculate();
+                    break;
+                case 4:
+                    Gcd.divisor();
+                    break;
                 default:
             }
         } catch (Exception e) {
