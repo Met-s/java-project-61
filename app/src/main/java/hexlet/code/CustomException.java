a50f6875ae3459668541b7d9914e7c33a21d75ac
Met-s <den_va@mail.ru>
2025-03-02 13:35:21 +0300

    In corect

--- app/src/main/java/hexlet/code/CustomException.java
+++ app/src/main/java/hexlet/code/CustomException.java
@@ -1,6 +1,6 @@
 package hexlet.code;
 
-import static java.lang.Integer.parseInt;
+//import static java.lang.Integer.parseInt;
 
 
 public class CustomException extends Exception {
@@ -8,23 +8,23 @@ public class CustomException extends Exception {
         super(message);
     }
 
-    public static void doSomething(String value) throws CustomException {
-        int minGame = 0;
-        int maxGame = 6;
-
-        try {
-            parseInt(value);
-        } catch (NumberFormatException e) {
-            throw new CustomException("Invalid value entered!\n"
-                    + "Value must be between 0 and 6!");
-        }
-
-        int value1 = parseInt(value);
-
-        if (value1 < minGame || value1 > maxGame) {
-            throw new CustomException("Value must be between 0 and 6!");
-        }
-    }
+//    public static void doSomething(String value) throws CustomException {
+//        int minGame = 0;
+//        int maxGame = 6;
+//
+//        try {
+//            parseInt(value);
+//        } catch (NumberFormatException e) {
+//            throw new CustomException("Invalid value entered!\n"
+//                    + "Value must be between 0 and 6!");
+//        }
+//
+//        int value1 = parseInt(value);
+//
+//        if (value1 < minGame || value1 > maxGame) {
+//            throw new CustomException("Value must be between 0 and 6!");
+//        }
+//    }
     public static void validateInput(String value) throws CustomException {
         if (value == null || value.isEmpty()) {
             throw new CustomException("Value must not be empty!");
